58fc9e86523e8e98d3976fe24ba088eebafbc284
Jenn Pham <phamav@miamioh.edu>
2023-11-21 21:33:23 -0500

    commit

--- CSE489-589GameEngine2023/Project3.h
+++ CSE489-589GameEngine2023/Project3.h
@@ -5,48 +5,12 @@
 class Project3 : public Game
 {
 protected:
-	bool t_key = false;
-	std::shared_ptr<GameObject> sphereObject;
-	std::shared_ptr<GameObject> boxObject;
-	std::shared_ptr<GameObject> modelObject;
-
 	std::shared_ptr<CameraComponent> mainCamComp;
 	std::shared_ptr<CameraComponent> secondCamComp;
 
 	virtual void processGameInput()
 	{
 		Game::processGameInput(); // call to superclass
-
-		if (glfwGetKey(glfwGetCurrentContext(), GLFW_KEY_0)) {
-			SharedFog::setFogMode(0); // no fog
-		}
-		else if (glfwGetKey(glfwGetCurrentContext(), GLFW_KEY_1)) {
-			SharedFog::setFogMode(1); // linear fog
-		}
-		else if (glfwGetKey(glfwGetCurrentContext(), GLFW_KEY_2)) {
-			SharedFog::setFogMode(2); // exp fog
-		}
-		else if (glfwGetKey(glfwGetCurrentContext(), GLFW_KEY_3)) {
-			SharedFog::setFogMode(3); // exp two fog
-		}
-		else if (glfwGetKey(glfwGetCurrentContext(), GLFW_KEY_EQUAL)) {
-			SharedFog::setFogDensity(SharedFog::getFogDensity() + 0.001f); // add fog density
-		}
-		else if (glfwGetKey(glfwGetCurrentContext(), GLFW_KEY_MINUS)) {
-			SharedFog::setFogDensity(glm::max(SharedFog::getFogDensity() - 0.001f, 0.0f)); // reduce fog density
-		}
-		else if (glfwGetKey(glfwGetCurrentContext(), GLFW_KEY_PERIOD)) {
-			float newFogStart = SharedFog::getFogStart() + 5.0f;
-			if (newFogStart < SharedFog::getFogEnd()) {
-				SharedFog::setFogStart(newFogStart);
-			}
-		}
-		else if (glfwGetKey(glfwGetCurrentContext(), GLFW_KEY_COMMA)) {
-			float newFogStart = SharedFog::getFogStart() - 5.0f;
-			if (newFogStart >= 0.0f) { // Fog start should not be negative
-				SharedFog::setFogStart(newFogStart);
-			}
-		}
 	}
 
 	void loadScene() override
@@ -71,67 +35,30 @@ protected:
 		SharedLighting::setUniformBlockForShader(shaderProgram);
 		SharedFog::setUniformBlockForShader(shaderProgram);
 
-		//// ******* Light Objects ******
-
-		// Create the directional light game object and component
-		auto dirLightObject = std::make_shared<GameObject>();
-		auto dirLightComp = std::make_shared<DirectionalLightComponent>(GLFW_KEY_D); // 'D' key to toggle
-		
-		addChildGameObject(dirLightObject);
-
-		dirLightObject->setPosition(glm::vec3(20.0f, 10.0f, 0.0f));
-		dirLightComp->setAmbientColor(glm::vec4(0.2f, 0.2f, 0.2f, 1.0f));
-		dirLightComp->setDiffuseColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-		dirLightComp->setSpecularColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-		
-		dirLightObject->addComponent(dirLightComp);
-
-		// Create the positional light game object and component
-		auto posLightObject = std::make_shared<GameObject>();
-		auto posLightComp = std::make_shared<PositionalLightComponent>(GLFW_KEY_P); // 'P' key to toggle
-		
-		addChildGameObject(posLightObject);
-
-		posLightObject->setPosition(glm::vec3(10.0f, 10.0f, 10.0f));
-		posLightComp->setAmbientColor(glm::vec4(0.2f, 0.2f, 0.2f, 1.0f));
-		posLightComp->setDiffuseColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f)); // Bright white light
-		posLightComp->setSpecularColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-
-		posLightObject->addComponent(posLightComp);
-
-		// Create the spotlight game object and component
-		auto spotLightObject = std::make_shared<GameObject>();
-		auto spotLightComp = std::make_shared<SpotLightComponent>(GLFW_KEY_S); // 'S' key to toggle
-		
-		addChildGameObject(spotLightObject);
-
-		spotLightObject->setPosition(glm::vec3(0.0f, 0.0f, 10.0f)); // Adjust position as needed
-		spotLightComp->setSpotDirection(glm::vec3(0.0f, 0.0f, -2.0f)); // Pointing towards the objects
-		spotLightComp->setSpotCutoffCos(glm::cos(glm::radians(50.0f))); // Wider angle for the spotlight
-		spotLightComp->setSpotExponent(50.0f); // Softer falloff
-		
-		spotLightObject->addComponent(spotLightComp);
+		//// ******* Cameras **********
 
 		// Add the main (front) camera
 		auto mainCamObj = std::make_shared<GameObject>();
 		mainCamComp = std::make_shared<CameraComponent>(0); // depth 0
 		addChildGameObject(mainCamObj);
 
-		mainCamObj->setPosition(vec3(0.0f, 0.0f, 20.0f));
+		mainCamObj->setPosition(vec3(0.0f, 500.0f, 0.0f));
+		mainCamObj->rotateTo(vec3(0.0f, -1.0f, 0.0f));
+		mainCamComp->setViewPort(0.0f, 0.5f, 1.0f, 0.5f);
 		mainCamObj->addComponent(std::make_shared<ArrowRotateComponent>());
 		mainCamObj->addComponent(mainCamComp);
 
 		// Add a second camera
-		auto secondCamObj = std::make_shared<GameObject>();
-		secondCamComp = std::make_shared<CameraComponent>(1, 90.0f);
-		addChildGameObject(secondCamObj);
+		//auto secondCamObj = std::make_shared<GameObject>();
+		//secondCamComp = std::make_shared<CameraComponent>(1, 90.0f);
+		//addChildGameObject(secondCamObj);
 
-		secondCamObj->setPosition(vec3(-15.0f, 0.0f, 20.0f));
-		secondCamObj->rotateTo(ZERO_V3 - vec3(-15.0f, 0.0f, 20.0f));
-		secondCamObj->setPosition(vec3(-15.0f, 0.0f, 20.0f));
+		//secondCamObj->setPosition(vec3(-15.0f, 0.0f, 20.0f));
+		//secondCamObj->rotateTo(ZERO_V3 - vec3(-15.0f, 0.0f, 20.0f));
+		//secondCamObj->setPosition(vec3(-15.0f, 0.0f, 20.0f));
 
-		secondCamComp->setViewPort(0.6f, 0.6f, 0.4f, 0.4f);
-		secondCamObj->addComponent(secondCamComp);
+		//secondCamComp->setViewPort(0.6f, 0.6f, 0.4f, 0.4f);
+		//secondCamObj->addComponent(secondCamComp);
 
 		//// ***** Sound Setup *****
 
@@ -158,37 +85,79 @@ protected:
 
 		// ***** Sun ******
 		auto sunGameObject = std::make_shared<GameObject>();
-		this->addChildGameObject(sunGameObject);
+		addChildGameObject(sunGameObject);
 		sunGameObject->setPosition(vec3(0.0f, 0.0f, 0.0f), WORLD);
 
 		Material sunMat;
-		sunMat.setEmissiveMat(YELLOW_RGBA);
-		auto sunMesh = std::make_shared<SphereMeshComponent>(shaderProgram, sunMat);
+		sunMat.setDiffuseTexture(Texture::GetTexture("Textures/sun.jfif")->getTextureObject());
+		sunMat.setTextureMode(DECAL);
+		auto sunMesh = std::make_shared<SphereMeshComponent>(shaderProgram, sunMat, 20.0f);
 		sunGameObject->addComponent(sunMesh);
 
 		// Sun has a positional light source at its location
-		// TODO: create the positional light source
+		std::shared_ptr<GameObject> sunLight = std::make_shared<GameObject>();
+		addChildGameObject(sunLight);
+		sunLight->setPosition(vec3(0.0f, 0.0f, 0.0f));
+		sunLight->addComponent(std::make_shared<PositionalLightComponent>(GLFW_KEY_S));
 
-		// ***** Empty GameObject ****
-		auto emptyGameObject = std::make_shared<GameObject>();
-		addChildGameObject(emptyGameObject);
-		emptyGameObject->addComponent(std::make_shared<SpinComponent>());
 
-		// ***** Empty GameObject 2 ****
-		auto emptyGameObject2 = std::make_shared<GameObject>();
-		addChildGameObject(emptyGameObject2);
-		emptyGameObject2->setPosition(vec3(-50.0f, 0.0f, 0.0f), LOCAL);
-
-		// ***** Earth *****
+		// ***** Earth and Moon *****
 		auto earthGameObject = std::make_shared<GameObject>();
-		emptyGameObject2->addChildGameObject(earthGameObject);
+		auto earthOrbitSun = std::make_shared<GameObject>();
+		auto moonGameObject = std::make_shared<GameObject>();
+		auto moonOrbitEarth = std::make_shared<GameObject>();
 
+		addChildGameObject(earthOrbitSun);
+		earthOrbitSun->addChildGameObject(earthGameObject);
+		earthOrbitSun->addChildGameObject(moonOrbitEarth);
+		moonOrbitEarth->addChildGameObject(moonGameObject);
+
+		// Apply texture for Earth
 		Material earthMat;
-		earthMat.setDiffuseTexture(Texture::GetTexture("Textures/earthmap.jpg")->getTextureObject());
-		auto earthMesh = std::make_shared<SphereMeshComponent>(shaderProgram);
+		earthMat.setDiffuseTexture(Texture::GetTexture("Textures/Terrestrial1.png")->getTextureObject());
+		auto earthMesh = std::make_shared<SphereMeshComponent>(shaderProgram, earthMat, 8.0f);
 		earthGameObject->addComponent(earthMesh);
 		earthGameObject->addComponent(std::make_shared<SpinComponent>(UNIT_Y_V3, 90.0f));
-		
+
+		// Apply texture for Moon
+		Material moonMat;
+		moonMat.setDiffuseTexture(Texture::GetTexture("Textures/moon.jpg")->getTextureObject());
+		auto moonMesh = std::make_shared<SphereMeshComponent>(shaderProgram, moonMat, 3.0f);
+		moonGameObject->addComponent(moonMesh);
+		moonGameObject->addComponent(std::make_shared<SpinComponent>(UNIT_Y_V3, 45.0f)); // Moon's spin
+
+		// Configure Earth's and Moon's positions
+		earthOrbitSun->setPosition(vec3(0.0f, 0.0f, 0.0f));
+		earthGameObject->setPosition(vec3(-50.0f, 0.0f, 0.0f));
+		moonOrbitEarth->setPosition(earthGameObject->getPosition());
+		moonGameObject->setPosition(vec3(-75.0f, 0.0f, 0.0f));
+
+		// Configure Earth's and Moon's orbital motion
+		earthOrbitSun->addComponent(std::make_shared<SpinComponent>(UNIT_Y_V3, 5.0f)); // Earth's orbit
+		moonOrbitEarth->addComponent(std::make_shared<SpinComponent>(UNIT_Y_V3, 12.0f)); // Moon's orbit
+
+		earthGameObject->addComponent(std::make_shared<SpinComponent>(UNIT_Y_V3, 20.0f));
+		moonGameObject->addComponent(std::make_shared<SpinComponent>(UNIT_Y_V3, 10.0f));
+
+		// ***** Jupiter *****
+		auto jupiterGameObject = std::make_shared<GameObject>();
+		auto jupiterOrbitSun = std::make_shared<GameObject>();
+		addChildGameObject(jupiterOrbitSun); // Jupiter's orbit around the Sun
+		jupiterOrbitSun->addChildGameObject(jupiterGameObject); // Jupiter
+
+		// Configure Jupiter
+		Material jupiterMat;
+		jupiterMat.setDiffuseTexture(Texture::GetTexture("Textures/Gaseous1.png")->getTextureObject());
+		auto jupiterMesh = std::make_shared<SphereMeshComponent>(shaderProgram, jupiterMat, 10.0f);
+		jupiterGameObject->addComponent(jupiterMesh);
+
+		// Configure Jupiter's orbital motion and position
+		jupiterOrbitSun->setPosition(vec3(0.0f, 0.0f, 0.0f));
+		jupiterOrbitSun->addComponent(std::make_shared<SpinComponent>(UNIT_Y_V3, 3.0f)); // Jupiter's orbit
+
+		jupiterGameObject->setPosition(vec3(-100.0f, 0.0f, 0.0f));
+		jupiterGameObject->addComponent(std::make_shared<SpinComponent>(UNIT_Y_V3, 5.0f)); // Jupiter's spin
+
 
 	} // end loadScene
 
